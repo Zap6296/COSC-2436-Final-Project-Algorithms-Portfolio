18d28794d9fa480cad77f8860cbc7c350e337507
Zap6296 <120212519+Zap6296@users.noreply.github.com>
2026-05-10 16:31:07 -0500

    Add files via upload

---
+++ CH04/main.py
@@ -0,0 +1,40 @@
+def quicksort(array):
+    """
+    Sort an array using the quicksort algorithm.
+    
+    Args:
+        array: List of numbers to sort
+        
+    Returns:
+        Sorted list
+    """
+    # TODO: Implement quicksort
+    # 1. Check if array length is less than 2 for base case
+    # 2. Select pivot as the first element
+    # 3. Partition: elements less than or equal to pivot
+    # 4. Partition: elements greater than pivot
+    # 5. Combine: quicksort(less) + [pivot] + quicksort(greater)
+    if len(array) <= 1:
+        return array
+    else:
+        pivot = array.pop(0)
+        greater = []
+        less = []
+        for x in range(len(array)):
+            print(x)
+            if (array[x] > pivot):
+                greater.append(array[x])
+                
+            if array[x] <= pivot:
+                less.append(array[x])
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+
+if __name__ == "__main__":
+    # Test cases
+    print(quicksort([10, 5, 2, 3]))
+    print(quicksort([33, 15, 10]))
+    print(quicksort([3, 5, 2, 1, 4]))
+    print(quicksort([1]))
+    print(quicksort([]))
+    print(quicksort([8, 7, 6, 5, 4, 3, 2, 1]))
